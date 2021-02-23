8138ca338a39ff5ae2f1d352e4f710b5c9296389
yuki <yk.yuki21@gmail.com>
2021-02-24 00:02:32 +0900

    ユニット詳細のエンドポイントを追加

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,4 +3,14 @@ class UnitsController < ApplicationController
     units = Unit.all
     render status: 200, json: units, each_serializer: UnitSerializer
   end
+
+  def show
+    unit = Unit.find_by(id: params[:id])
+
+    if unit
+      render status: 200, json: unit, serializer: UnitSerializer
+    else
+      render status: 404
+    end
+  end
 end
